d13a3a278fc098dc2c2481be1ccbc1895196ed8c
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-07-06 18:52:30 +0300

    Remove integration test files

--- src/test/java/com/biezbardis/bankingapp/BankingAppApplicationTests.java
+++ src/test/java/com/biezbardis/bankingapp/BankingAppApplicationTests.java
@@ -1,10 +1,10 @@
 package com.biezbardis.bankingapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
+@Disabled
 @SpringBootTest
 class BankingAppApplicationTests {
 
--- src/test/java/com/biezbardis/bankingapp/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.biezbardis.bankingapp;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
